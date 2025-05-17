560354eb84f19fe0ffdde87d8006e97cc931dc5e
Dan Jaheny <danjaheny@mac.home>
2025-05-17 22:31:01 +0200

    cleanup

--- psbt_decoder.py
+++ psbt_decoder.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
 import subprocess
 import base64
@@ -10,18 +9,14 @@ if len(sys.argv) != 2:
 
 hex_psbt = sys.argv[1].lower()
 
-# Strip the "0x" prefix if present
 if hex_psbt.startswith("0x"):
     hex_psbt = hex_psbt[2:]
 
-# Convert from hex to binary
 binary_psbt = bytes.fromhex(hex_psbt)
 
-# Encode that binary data into base64
 base64_psbt = base64.b64encode(binary_psbt).decode()
 print(base64_psbt)
 
-# Decode the PSBT with bitcoin-cli
 try:
     result = subprocess.check_output(["bitcoin-cli", "decodepsbt", base64_psbt])
     print(result.decode())
